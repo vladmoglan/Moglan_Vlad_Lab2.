6d37a21f74686c1eecefb5c28e1f4deb74e409df
Vlad <Vlad@DESKTOP-LDNTOVL>
2021-10-14 17:05:52 +0300

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moglan_Vlad_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public ICollection<Order> Orders { get; set; }
+
+    }
+}
