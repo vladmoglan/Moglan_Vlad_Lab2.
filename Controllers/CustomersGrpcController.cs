e34ce15d968fd9d602afe26761b8948a3237d786
vladmoglan <moglanvladandrei@gmail.com>
2022-01-02 20:41:18 +0200

    Laboratorul 11

---
+++ Controllers/CustomersGrpcController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using GrpcCustomersService;
+
+namespace Moglan_Vlad_Lab2.Controllers
+{
+    public class CustomersGrpcController : Controller
+    {
+        private readonly GrpcChannel channel;
+        public CustomersGrpcController()
+        {
+            channel = GrpcChannel.ForAddress("https://localhost:5001");
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            CustomerList cust = client.GetAll(new Empty());
+            return View(cust);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new CustomerService.CustomerServiceClient(channel);
+                client.Insert(customer);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return BadRequest();
+        }
+    }
+}
